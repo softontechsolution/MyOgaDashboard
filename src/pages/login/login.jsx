ce3ea3b065a2569dd89130bd79323d98fbf5095c
softontechsolution <softontechsolution@gmail.com>
2023-04-02 00:57:51 +0100

    login updated

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,7 @@
 import "./login.scss";
 import { useContext, useState, useRef } from "react";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { collection, where, query, getDocs } from "firebase/firestore";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
@@ -19,25 +20,37 @@ const Login = () => {
     const { dispatch } = useContext(AuthContext)
 
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                dispatch({ type: "LOGIN", payload: user })
-                setMsg("Logged In Succesfully");
-                setType("success");
-                snackbarRef.current.show();
-                navigate("/");
-            })
-            .catch((error) => {
-                setError(true);
-                setMsg(error.message);
-                setType("error");
-                snackbarRef.current.show();
-            });
+        const adminRef = collection(db, "Admin");
+        const q = query(
+            adminRef,
+            where("email", "==", email),
+            where("password", "==", password)
+        );
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    dispatch({ type: "LOGIN", payload: user })
+                    setMsg("Logged In Succesfully");
+                    setType("success");
+                    snackbarRef.current.show();
+                    navigate("/");
+                })
+                .catch((error) => {
+                    setError(true);
+                    setMsg(error.message);
+                    setType("error");
+                    snackbarRef.current.show();
+                });
+        } else {
+            alert("You Don't Have Permission");
+        }
+
 
 
     }
